8573d01357f78c1140fb20b5199fa55c5e2bccfa
Anthony Lu <anthony.x.lu@pwc.com>
2019-05-17 17:44:37 +0800

    up

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "no-console": "off",
+        "indent":[
+            "error",
+            4
+        ],
+        "quotes":[
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
---
+++ test.js
@@ -0,0 +1,15 @@
+var a={
+    a:'dsfa',
+    b:'dfaskl'
+};
+const b = 'af';
+
+
+var c = {
+    a: 'dsfa',
+    b: 'dfaskl'
+};
+console.log('fasas');
+console.log(a);
+console.log(b);
+console.log(c);
