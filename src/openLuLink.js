43a660b30ba97390f621a87aa177c02105aebf59
Your Name <you@example.com>
2019-05-31 21:07:16 +0800

    update

--- src/openLuLink.js
+++ src/openLuLink.js
@@ -6,9 +6,9 @@ let seoUrlArr = ['www.movingboxsale.com','www.ausplastic.com']
 let inputID = 'dn'
 let startBtn = 'linkbtn'
 
-// new CronJob('00 00 */9 * * *', function () {
-new CronJob('00 */1 * * * *', function () {
-    console.log('You will see this message every minute');
+new CronJob('00 00 */9 * * *', function () {
+// new CronJob('00 */1 * * * *', function () {
+    console.log('---cron triggered---');
     console.log(new Date());
     seoUrlArr.forEach(seoUrl=>{
       openSeoPage(seoPlatformUrl, seoUrl, inputID, startBtn)
