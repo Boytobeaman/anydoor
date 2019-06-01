f20bdaa5bdbc26403efad35e15889ab528952d04
Your Name <you@example.com>
2019-06-01 16:54:53 +0800

    ts

--- src/openLuLink.js
+++ src/openLuLink.js
@@ -1,10 +1,15 @@
 var CronJob = require('cron').CronJob;
 const openSeoPage = require('./helper/openPageSearch')
 
-let seoPlatformUrl = 'https://tool.lusongsong.com/seo/'
-let seoUrlArr = ['www.movingboxsale.com','www.ausplastic.com']
-let inputID = 'dn'
-let startBtn = 'linkbtn'
+// let seoPlatformUrl = 'https://tool.lusongsong.com/seo/'
+// let seoUrlArr = ['www.movingboxsale.com','www.ausplastic.com']
+// let inputID = 'dn'
+// let startBtn = 'linkbtn'
+
+let seoPlatformUrl = 'https://www.ausplastic.com'
+let seoUrlArr = ['folding crates','stacking crate','moving boxes']
+let inputID = '.product-header-search'
+let startBtn = 'button[type="submit"]'
 
 // new CronJob('00 00 */9 * * *', function () {
 new CronJob('00 */1 * * * *', function () {
