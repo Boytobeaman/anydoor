2be58a6d9247d3eaea2bd7851e0843f26ff9198f
Your Name <you@example.com>
2019-06-01 10:42:12 +0800

    up

--- src/helper/openPageSearch.js
+++ src/helper/openPageSearch.js
@@ -8,27 +8,29 @@ module.exports = async (seoPlatformUrl, seoUrl, inputID, startBtn) =>{
     // const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(seoPlatformUrl);
-    console.log(`go to: ${seoPlatformUrl}`);
+    console.log(`go to: ${seoPlatformUrl} for (${seoUrl})`);
     await page.setViewport({
         width: 1920,
         height: 1300
     });
     console.log(`reset viewport`);
     await page.waitFor(1200);
-    await page.focus(`#${inputID}`);
-    // select input text to remove them
-    await page.keyboard.down('Control');
-    await page.keyboard.down('A');
-    await page.keyboard.up('A');
-    await page.keyboard.up('Control');
-
-    await page.keyboard.sendCharacter(seoUrl);
-    await page.click(`#${startBtn}`);
-    console.log(`started for url ${seoUrl}`);
+    
     page.on('load', async () =>{
-        setTimeout(()=>{
-        },1*60*60*1000)
+      await page.focus(`#${inputID}`);
+      // select input text to remove them
+      await page.keyboard.down('Control');
+      await page.keyboard.down('A');
+      await page.keyboard.up('A');
+      await page.keyboard.up('Control');
+
+      await page.keyboard.sendCharacter(seoUrl);
+      await page.click(`#${startBtn}`);
+      console.log(`started for url ${seoUrl}`);
+      setTimeout(()=>{
         await browser.close();
+      },1*60*60*1000)
+      
     });
   } catch (error) {
       console.log(error);
