6737e55440f97eda80b809396c9925373e8544be
Your Name <you@example.com>
2019-06-01 16:45:48 +0800

    add ausplastic test

---
+++ src/ausplastic.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer');
+const {mn} = require('./config/ssConfig');
+const srcToImg = require('./helper/srcToImg');
+
+const to_url = `https://www.ausplastic.com`;
+(async () => {
+    try {
+        // const browser = await puppeteer.launch({headless: false});
+        const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+        const page = await browser.newPage();
+        await page.goto(to_url);
+        console.log(`go to: ${to_url}`);
+        await page.setViewport({
+            width: 1920,
+            height: 2300
+        });
+        console.log(`reset viewport`);
+        await page.focus('.product-header-search');
+        await page.keyboard.sendCharacter('stackable');
+        await page.click('button[type="submit"]');
+        console.log('go to search list');
+        page.on('load', async () =>{
+          console.log('page loading done, start fetch...');
+          await page.waitFor(22000);
+          await browser.close();
+        });
+        
+    } catch (error) {
+        console.log(error);
+    }
+    
+})();
