cf92339c2b685cea4142225460c2b9d25a818570
Your Name <you@example.com>
2019-06-08 09:40:58 +0800

    update

--- src/helper/pingPage.js
+++ src/helper/pingPage.js
@@ -3,10 +3,10 @@ const puppeteer = require('puppeteer');
 // const puppeteer = require('puppeteer-cn');
 
 module.exports = async (seoPlatformUrl, callback) =>{
+  // const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  // const browser = await puppeteer.launch();
   try {
-    // const browser = await puppeteer.launch({headless: false});
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-    // const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(seoPlatformUrl);
     console.log(`go to: ${seoPlatformUrl}`);
@@ -14,12 +14,13 @@ module.exports = async (seoPlatformUrl, callback) =>{
         width: 1300,
         height: 1300
     });
-    await page.waitFor(4200);
+    await page.waitFor(1200);
     browser.close();
     callback(null,"")
 
   } catch (error) {
       console.log(error);
+      browser.close();
       callback(null,"")
   }
 };
